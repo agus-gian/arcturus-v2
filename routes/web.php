a36ce34c3a50d881b351ec3b6cdec23b90704a34
All Good Bud AGB <appdev@allgoodbud.com>
2024-05-28 21:56:12 +0800

    second commit

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/welcome', function () {
+    return \Inertia\Inertia::render('Welcome',[
+        'user' => [
+            'name' => 'John Doe',
+            'email' => 'agus@email.com'
+        ]
+    ]);
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,13 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/sass/app.scss',
-                'resources/js/app.js',
+                'resources/js/auth.js',
             ],
             refresh: true,
+            resolve: name => {
+                const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
+                return pages[`./Pages/${name}.jsx`]
+            },
         }),
     ],
 });
